b2744317e58272ae966afe133ec7c69f888b03ca
MuhammadNoor <noor4apps@gmail.com>
2022-09-03 19:23:10 +0300

    api categories module admin

---
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::select('id', 'name')->get();
+
+        if ($categories) {
+            $data = CategoryResource::collection($categories)->response()->getData(true);
+            return response()->json(['data' => $data, 'error' => 0, 'message' => ''], 200);
+        } else {
+            return response()->json(['data' => null, 'error' => 1, 'message' => 'something went wrong!'], 201);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191|unique:categories',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['data' => null, 'error' => 1, 'message' => $validator->errors()->first()], 201);
+        }
+
+        $params = $request->except('_token');
+
+        $category = Category::create($params);
+
+        if ($category) {
+            return response()->json(['data' => null, 'error' => 0, 'message' => 'Category added successfully.'], 200);
+        }
+
+        return response()->json(['data' => null, 'error' => 1, 'message' => 'Error occurred while creating category.'], 201);
+    }
+
+    public function edit($category)
+    {
+        $category = Category::whereId($category)->first();
+
+        if ($category) {
+            $data['category'] = new CategoryResource($category);
+            return response()->json(['data' => $data, 'error' => 0, 'message' => ''], 200);
+        }
+        return response()->json(['data' => null, 'error' => 1, 'message' => 'something went wrong!'], 201);
+    }
+
+    public function update(Request $request, $category)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191|unique:categories,name,' . $category,
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['data' => null, 'error' => 1, 'message' => $validator->errors()->first()], 201);
+        }
+
+        $params = $request->except('_token');
+
+        $category = Category::whereId($category)->first();
+
+        if ($category) {
+            $category->update($params);
+            return response()->json(['data' => null, 'error' => 0, 'message' => 'Category updated successfully.'], 200);
+        }
+
+        return response()->json(['data' => null, 'error' => 1, 'message' => 'Error occurred while updating category.'], 201);
+    }
+
+    public function destroy($category)
+    {
+        $category = Category::whereId($category)->first();
+
+        if ($category) {
+            $category->delete();
+            return response()->json(['data' => null, 'error' => 0, 'message' => 'Category deleted successfully.'], 200);
+        }
+
+        return response()->json(['data' => null, 'error' => 1, 'message' => 'Error occurred while deleting category.'], 201);
+    }
+}
--- routes/api/admin.php
+++ routes/api/admin.php
@@ -1,10 +1,19 @@
 <?php
 
 use App\Http\Controllers\Api\Admin\BookController;
+use App\Http\Controllers\Api\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth:sanctum', 'is_admin']], function () {
+
+    Route::group(['prefix' => 'categories'], function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('admin.categories.index');
+        Route::post('/', [CategoryController::class, 'store'])->name('admin.categories.store');
+        Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('admin.categories.edit');
+        Route::patch('/{category}', [CategoryController::class, 'update'])->name('admin.categories.update');
+        Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('admin.categories.destroy');
+    });
 
     Route::group(['prefix' => 'books'], function () {
         Route::get('/', [BookController::class, 'index'])->name('admin.books.index');
